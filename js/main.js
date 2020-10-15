ef1bfdd7f17e33d8cd4e101a3526253401697dbf
AlexSzczesnowicz <193945w@gmail.com>
2020-10-15 12:59:15 +0200

    if else project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let i = 12;
+let msg_1 = 'Podana liczba';
+let msg_2 = 'jest większa od 10';
+let msg_3 = ' ';
+let msg_4 ='';
+if (i >= 10){ 
+    msg_3 = ' ';
+} else {
+    msg_3 = ' nie ';
+
+}
+msg_4 = msg_1+msg_3+msg_2;
+console.log(msg_4);
